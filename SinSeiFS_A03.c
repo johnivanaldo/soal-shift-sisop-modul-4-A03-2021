7e86a193c6368c78c6e88ae77711e243c3b81240
Rizarize <81413071+Rizarize@users.noreply.github.com>
2021-06-12 14:25:49 +0700

    Update SinSeiFS_A03.c

--- SinSeiFS_A03.c
+++ SinSeiFS_A03.c
@@ -1,3 +1,22 @@
+#define FUSE_USE_VERSION 28
+#include <errno.h>
+#include <sys/time.h>
+#include <wait.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <ctype.h>
+#include <time.h>
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+
+static const char *dirpath = "/home/[user]/Downloads";
+
 int main(int argc, char *argv[])
 {
     umask(0);
